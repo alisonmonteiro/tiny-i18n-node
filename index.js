f34be775463c1b42c66204a3cee589d09e890632
Alison Monteiro <alisonmonteiro.10@gmail.com>
2019-06-21 15:15:45 -0300

    Start exposing a simple translator method.
    Internally also set the default locale, file location, and default value.

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
-module.exports = term => {
-  return term
+const fs = require('fs')
+const path = require('path')
+
+const isString = val => (
+  typeof val === 'string' || Object.prototype.toString.call(val) === '[object String]'
+)
+
+const isPlainObject = val => (
+  Object.prototype.toString.call(val) === '[object Object]'
+)
+
+const hasOwnProp = (obj, key) => (
+  Object.prototype.hasOwnProperty.call(obj, key)
+)
+
+class Trans {
+  constructor() {
+    this.locale = 'en'
+    this.file = './lang'
+    this.defaultValue = null
+  }
+
+  normalizeKeys(keys, separator = '.') {
+    const valid = isString(keys) ? keys : ''
+
+    return valid.split(separator)
+  }
+
+  getEntries(translations, keys) {
+    const keysList = this.normalizeKeys(keys)
+
+    return keysList.reduce((result, key) => {
+      if (isPlainObject(result) && hasOwnProp(result, key)) {
+        return result[key]
+      }
+
+      return null
+    }, translations)
+  }
+
+  translator(term) {
+    const file = `${path.resolve(this.file)}/${this.locale}.json`
+    try {
+      const data = fs.readFileSync(file)
+      const content = JSON.parse(data.toString('utf8'))
+
+      return this.getEntries(content, term) || this.defaultValue
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.error(`File ${file} not found!`)
+        return false
+      }
+
+      throw error
+    }
+  }
+
+  setLocale(locale) {
+    this.locale = locale
+  }
+
+  setDefaultValue(val) {
+    const validDefault = isString(val) ? val : ''
+    this.defaultValue = validDefault
+  }
 }
+
+module.exports = function (term, defaultValue = '') {
+  const trans = new Trans()
+  trans.setDefaultValue(defaultValue)
+
+  return trans.translator(term)
+}
+
--- test.js
+++ test.js
@@ -1,8 +1,44 @@
 import test from 'ava'
 import m from '.'
 
-test('translate term', t => {
-  const term = 'simple'
+test('try to translate invalid terms', t => {
+  const termString = 'invalid string'
+  const expectString = ''
 
-  t.is(m(term), term, 'Simple term translation')
+  const termUndefined = undefined
+  const expectUndefined = ''
+
+  const termObject = {}
+  const expectObject = ''
+
+  t.is(m(termString), expectString)
+  t.is(m(termUndefined), expectUndefined)
+  t.is(m(termObject), expectObject)
+})
+
+test('try to translate invalid terms but with default', t => {
+  const termDefault = 'Oops!'
+  const invalidTermWithDefault = undefined
+
+  const termInvalidDefaultCorrection = ''
+  const termInvalidDefault = {}
+  const invalidTermWithInvalidDefault = undefined
+
+  t.is(m(invalidTermWithDefault, termDefault), termDefault)
+  t.is(m(invalidTermWithInvalidDefault, termInvalidDefault), termInvalidDefaultCorrection)
+})
+
+test('translate plain and not plain', t => {
+  const term = 'test'
+  const expect = 'Test'
+
+  const termTwo = 'social.twitter'
+  const expectTwo = 'twitter.com/twitter'
+
+  const termThree = 'footer.address.zip'
+  const expectThree = '60000'
+
+  t.is(m(term), expect)
+  t.is(m(termTwo), expectTwo)
+  t.is(m(termThree), expectThree)
 })
